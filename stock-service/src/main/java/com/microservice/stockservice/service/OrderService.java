277f1314bdd673eaf0ad70f1bc48bb8ab140243d
Subhra <subhrajyotisahoo2001.com>
2025-06-19 16:38:15 +0530

    Added redis

--- stock-service/src/main/java/com/microservice/stockservice/service/OrderService.java
+++ stock-service/src/main/java/com/microservice/stockservice/service/OrderService.java
@@ -3,14 +3,18 @@ package com.microservice.stockservice.service;
 import com.microservice.stockservice.dto.Order;
 import com.microservice.stockservice.entity.OrderEntity;
 import com.microservice.stockservice.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OrderService {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
     @Autowired
     private OrderRepository orderRepository;
 
@@ -20,6 +24,7 @@ public class OrderService {
         entity.setName(order.getName());
         entity.setQty(order.getQty());
         entity.setPrice(order.getPrice());
+        LOGGER.info("Saving order to DB: {}", entity);
         return orderRepository.save(entity);
     }
 
@@ -27,11 +32,15 @@ public class OrderService {
         return orderRepository.findAll();
     }
 
+    @Cacheable(value = "orders", key = "#id")
     public OrderEntity getOrderById(String id) {
+        LOGGER.info("Fetching order by ID {} from DB", id);
         return orderRepository.findById(id).orElse(null);
     }
 
+    @CacheEvict(value = "orders", key = "#id")
     public void deleteOrder(String id) {
+        LOGGER.info("Deleting order with ID: {}", id);
         orderRepository.deleteById(id);
     }
 }
